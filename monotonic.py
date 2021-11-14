3f322ce1492e1176848ec3385133e860b4de5b9e
muaddibA <92188103+muaddibA@users.noreply.github.com>
2021-11-14 13:17:09 +0200

    Update monotonic.py

--- monotonic.py
+++ monotonic.py
@@ -1,16 +1,19 @@
 nums = list(map(int,input('Введите элементы списка nums через пробел :').split()))
- 
+
 def is_monotonic(nums):
- monotone_rising_flag = True;
- monotone_decreasing_flag = True;
+    count = 1
     for i in range (0, len(nums) - 1):
-        if (nums[i] >=> nums[i + 1]):
- monotone_rising_flag = False;
         if (nums[i] <= nums[i + 1]):
- monotone_decreasing_flag = False;
-    if (monotone_decreasing_flag == False and monotone_rising_flag == False):
-        return False;
-    return True;
+            count += 1
+    if (count == len(nums)):
+        return True
+    count = 1
+    for i in range (0, len(nums) - 1):
+        if (nums[i] >= nums[i + 1]):
+            count += 1
+    if (count == len(nums)):
+        return True
+    return False
     
 if (is_monotonic(nums)):
     print("True")
